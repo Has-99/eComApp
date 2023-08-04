45c8f9f5628590956c46620acb8a0f97b481a0be
Has-99/ <kavi99hd@gmail.com>
2023-08-04 08:46:00 +0530

    adding cart

---
+++ controllers/cart.js
@@ -0,0 +1,103 @@
+const Cart = require('../models/cart');
+const Product = require('../models/product')
+
+class cartController{
+
+    getAllCarts = async (req, res, next) => {
+        try {
+            const carts = await Cart.find({});
+            res.status(200).json(carts);
+        } catch(e) {
+            res.status(500).json({message: e.message})
+        }
+    };
+
+    addCart = async(req, res) => {
+        try{
+            const cart = await Cart.create(req.body)
+            res.status(200).json(cart)
+        } catch(e) {
+            console.log(e.message)
+            res.status(500).json({message: e.message})
+        }
+    };
+
+    placeCart = async(req, res) => {
+        try {
+            // console.log(req.body.owner);
+            const cart = await Cart.findOneAndUpdate({owner: req.body.owner}, {placed: true});
+            if(!cart){
+                return res.status(404).json({message: 'Cannot find Product'})
+            }
+            // const newCart = await Cart.findById(id);
+            res.status(200).json({success: true});
+        } catch(e) {
+            res.status(500).json({message: e.message})
+        }
+    };
+
+    addToCart = async(req, res) => {
+        try {
+            const product = await Product.findOne({name:req.body.product});
+            if(product.quantity >= req.body.quantity){
+                const result0 = await Product.updateOne({name:req.body.product}, {$inc: { quantity: -(req.body.quantity) }});
+
+                const query = { owner:req.body.owner, "products.product": req.body.product };
+                const updateDocument = {$inc: { "products.$.quantity": req.body.quantity }};
+                const result = await Cart.updateOne(query, updateDocument);
+
+                if(result.modifiedCount==0){
+                    const result2 = await Cart.updateOne({ owner:req.body.owner }, {
+                        $push: {
+                            products: {product:req.body.product, quantity:req.body.quantity, price: req.body.price}
+                        }
+                    });
+                }
+                res.status(200).json({success:true});
+            } else {
+                res.status(200).json({success:false, message:'not enough stock'});
+            }
+        } catch(e) {
+            res.status(500).json({message: e.message})
+        }
+    };
+
+    removeFromCart = async(req,res) => {
+        try{
+            const query = { owner:req.body.owner};
+            const updateDocument = { $pull: { 'products': { product: req.body.product } } };
+            const result = await Cart.updateOne(query, updateDocument);
+
+            const result1 = await Product.updateOne({'name': req.body.product}, {$inc: { "quantity": req.body.quantity }});
+
+            res.status(200).json((await Cart.findOne({owner: req.body.owner})).products);
+        } catch(e) {
+            res.status(500).json({message: e.message})
+        }
+    }
+
+    updateQuantity = async(req, res) => {
+        try {
+            const query = { owner:req.body.owner, "products.product": req.body.product };
+            const updateDocument = { "products.$.quantity": req.body.quantity };
+            const result = await Cart.updateOne(query, updateDocument);
+
+            res.status(200).json(result);
+        } catch(e) {
+            res.status(500).json({message: e.message})
+        }
+    };
+
+    getCart = async (req, res, next) => {
+        try {
+            // console.log(req.body);
+            const cart = await Cart.findOne({owner: req.body.owner});
+            // console.log(cart);
+            res.status(200).json(cart.products);
+        } catch(e) {
+            res.status(500).json({message: e.message})
+        }
+    };
+}
+
+module.exports = new cartController;
---
+++ routes/cart.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const cartController = require('../controllers/cart');
+const { isAuth } = require('../middlewares/auth');
+
+router.get('/getCarts', cartController.getAllCarts)
+router.post('/getCart', isAuth, cartController.getCart)
+router.post('/createcart', cartController.addCart)
+router.post('/updatequantity', cartController.updateQuantity)
+router.post('/addtocart', isAuth, cartController.addToCart)
+router.post('/removefromcart', isAuth, cartController.removeFromCart)
+router.put('/placecart', isAuth, cartController.placeCart)
+
+module.exports = router;
